0409141423f31522397bf46e19de91395fc1d181
Yuniari-Rafiah <rafiahyuniati@gmail.com>
2021-06-08 11:20:15 +0800

    deklarasi variabel dan tipe data

---
+++ src/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes {
+    public static void main(String[] args) {
+
+        String namaDepan = "Yuniati";
+        String namaBelakang = "Rafiah";
+        int usia = 20;
+        int targetTahunKuliah = 4;
+        double ipk = 3.389764512;
+        char nilaiAbjad = 'A';
+        boolean cantik = true;
+
+    }
+}
